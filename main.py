87856ad20944915b7e41cd01052aeb848cde8473
ana_maria.nastase <ana_maria.nastase@stud.acs.upb.ro>
2024-05-15 22:37:04 +0300

    fixed trigrams for lda

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ import yake
 from langdetect import detect, detect_langs, DetectorFactory
 import re
 
+STOP_WORDS = ['paper', 'present', 'propose', 'show', 'datum', 'people', 'result', 'solution', 'case', 'order',
+              'base', 'ieee', 'privacy', 'policy', 'new', 'old', 'context', 'high', 'different', 'research', 'type',
+              'approach', 'important', 'main', 'range', 'helpful', 'large', 'difficult', 'available', 'amount',
+              'useful', 'importance', 'article', 'abstract', 'scale', 'copyright', 'real', 'quality', 'demonstrate',
+              'inconvenience', 'benefit', 'unavailable', 'term', 'condition', 'interest', 'recent', 'obtain',
+              'title', 'jat', 'jats',
+              'organization', 'task', 'student', 'professor', 'teacher', 'university', 'workshop', 'study', 'text',
+              'conference']
+
 
 def get_authors_texts():
     """ make a dictionary containing a list of abstracts for each author """
@@ -27,7 +36,7 @@ def get_authors_texts():
 
 
 def extract_keywords(author_names):
-    """ extract topics for each author using yake """
+    """ extract keywords for each author using yake """
     # make a dictionary containing the combined abstracts for each author
     authors_texts = {}
     for author_id, texts in get_authors_texts().items():
@@ -39,23 +48,13 @@ def extract_keywords(author_names):
     # entities/words to remove
     remove_entities = ['PERSON', 'NORP', 'FAC', 'GPE', 'LOC', 'DATE', 'TIME', 'PERCENT', 'MONEY',
                        'QUANTITY', 'CARDINAL', 'ORDINAL']
-    stop_words = ['paper', 'present', 'propose', 'show', 'datum', 'people', 'result', 'solution', 'case', 'order',
-                  'base', 'ieee', 'privacy', 'policy', 'new', 'old', 'context', 'high', 'different', 'research', 'type',
-                  'approach', 'important', 'main', 'range', 'helpful', 'large', 'difficult', 'available', 'amount',
-                  'useful', 'importance', 'article', 'abstract', 'scale', 'copyright', 'real', 'quality', 'demonstrate',
-                  'inconvenience', 'benefit', 'unavailable', 'term', 'condition', 'interest', 'recent', 'obtain',
-                  'title', 'jat', 'jats',
-                  'organization', 'task', 'student', 'professor', 'teacher', 'university', 'workshop', 'study', 'text',
-                  'conference']
 
     for author_id, text in authors_texts.items():
         # preprocess text
         doc = nlp(text)
         transformed_text = ' '.join(
             [token.text for token in doc if token.ent_type_ not in remove_entities
-             and token.lemma_.lower() not in stop_words])
-
-        print(str(author_id) + " - " + author_names[author_id])
+             and token.lemma_.lower() not in STOP_WORDS])
 
         # get 15 keyphrases of max 3 words
         max_ngram = 3
@@ -65,8 +64,68 @@ def extract_keywords(author_names):
                                                     top=keywords_nr, features=None)
 
         keywords = custom_kw_extractor.extract_keywords(transformed_text)
+
+        print(str(author_id) + " - " + author_names[author_id])
+
         for kw in keywords:
             print(kw)
+
+        print()
+
+
+def extract_topic(author_names):
+    """ extract 1 topic containing 10 keywords for each author using LDA """
+    # make a dictionary containing the combined abstracts for each author
+    authors_texts = get_authors_texts()
+
+    # load spacy model
+    nlp = spacy.load('en_core_web_md')
+
+    # keep only adjectives and nouns
+    remove_pos = ['ADV', 'PRON', 'PART', 'DET', 'SPACE', 'NUM', 'SYM', 'ADP', 'VERB', 'CCONJ']
+    stop_words = ['paper', 'present', 'propose', 'datum', 'people', 'result', 'solution', 'case', 'order', 'base',
+                  'ieee', 'privacy', 'policy',
+                  'new', 'old', 'context', 'high', 'different', 'research', 'type', 'approach',
+                  'important', 'main', 'range',
+                  'helpful', 'large', 'difficult', 'available', 'amount', 'useful', 'importance', 'article', 'abstract',
+                  'scale', 'copyright',
+                  'real', 'quality', 'inconvenience', 'benefit', 'unavailable', 'term', 'condition', 'interest',
+                  'organization', 'use', 'conference', 'thank',
+                  'task', 'student', 'professor', 'teacher', 'university']
+    remove_entities = ['PERSON', 'NORP', 'FAC', 'GPE', 'LOC', 'LANGUAGE', 'DATE', 'TIME', 'PERCENT', 'MONEY',
+                       'QUANTITY', 'CARDINAL', 'ORDINAL']
+
+    for author_id, texts in authors_texts.items():
+        # preprocess texts
+        tokens = []
+        for abstract in texts:
+            doc = nlp(abstract)
+            t = [token.lemma_.lower() for token in doc if token.is_alpha and token.ent_type_ not in remove_entities
+                 and token.lemma_.lower() not in stop_words and token.pos_ not in remove_pos and not token.is_stop]
+            tokens.append(t)
+
+        # add bigrams to the token list
+        bigram = Phrases(tokens, min_count=2, delimiter=' ', threshold=1)
+        tokens = [bigram[text] for text in tokens]
+        trigram = Phrases(tokens, min_count=2, delimiter=' ', threshold=1)
+        tokens = [trigram[text] for text in tokens]
+
+        # remove single words
+        tokens = [[token for token in text if len(token.split(" ")) > 1] for text in tokens]
+
+        # create dictionary with gensim
+        dictionary = Dictionary(tokens)
+        # create corpus
+        corpus = [dictionary.doc2bow(text) for text in tokens]
+
+        # apply LDA
+        lda_model = LdaMulticore(corpus=corpus, id2word=dictionary, iterations=500, num_topics=1, workers=4, passes=200)
+        topics = lda_model.print_topics(-1)
+
+        print(str(author_id) + " - " + author_names[author_id])
+
+        for idx, topic in topics:
+            print(f"{idx}: {topic}")
 
         print()
 
@@ -79,4 +138,4 @@ if __name__ == '__main__':
     id_name_list = list(zip(authors['id'], authors['last_name'], authors['first_name']))
     author_names = {author_id: last_name + " " + first_name for author_id, last_name, first_name in id_name_list}
 
-    extract_keywords(author_names)
+    extract_topic(author_names)
