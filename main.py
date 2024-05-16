e7e1bca0220fd425bbbac12433c423759f909221
ana_maria.nastase <ana_maria.nastase@stud.acs.upb.ro>
2024-05-16 13:44:08 +0300

    added fastText instead of langdetect

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from gensim.models import LdaMulticore
 from gensim.models import LdaModel
 from gensim.models import CoherenceModel
 import yake
-from langdetect import detect, detect_langs, DetectorFactory
+import fasttext
 import re
 
 STOP_WORDS = ['paper', 'present', 'propose', 'show', 'datum', 'people', 'result', 'solution', 'case', 'order',
@@ -25,12 +25,16 @@ def get_authors_texts():
     """ make a dictionary containing a list of abstracts for each author """
     author_publication_pairs = list(zip(publications['user_id'], publications['abstract_text']))
 
-    authors_texts = {author_id: [] for author_id in authors["id"]}
+    # load fastText model
+    model = fasttext.load_model('lid.176.bin')
+
+    authors_texts = {author_id: "" for author_id in authors["id"]}
     for author_id, abstract in author_publication_pairs:
         if abstract and isinstance(abstract, str) and re.match('^(?=.*[a-zA-Z])', abstract):
-            language = detect(abstract)
+            predictions = model.predict(abstract)
+            language = predictions[0][0].replace('__label__', '')
             if language == 'en':
-                authors_texts[author_id].append(abstract)
+                authors_texts[author_id] = authors_texts[author_id] + '\n' + abstract
 
     return authors_texts
 
@@ -83,15 +87,6 @@ def extract_topic(author_names):
 
     # keep only adjectives and nouns
     remove_pos = ['ADV', 'PRON', 'PART', 'DET', 'SPACE', 'NUM', 'SYM', 'ADP', 'VERB', 'CCONJ']
-    stop_words = ['paper', 'present', 'propose', 'datum', 'people', 'result', 'solution', 'case', 'order', 'base',
-                  'ieee', 'privacy', 'policy',
-                  'new', 'old', 'context', 'high', 'different', 'research', 'type', 'approach',
-                  'important', 'main', 'range',
-                  'helpful', 'large', 'difficult', 'available', 'amount', 'useful', 'importance', 'article', 'abstract',
-                  'scale', 'copyright',
-                  'real', 'quality', 'inconvenience', 'benefit', 'unavailable', 'term', 'condition', 'interest',
-                  'organization', 'use', 'conference', 'thank',
-                  'task', 'student', 'professor', 'teacher', 'university']
     remove_entities = ['PERSON', 'NORP', 'FAC', 'GPE', 'LOC', 'LANGUAGE', 'DATE', 'TIME', 'PERCENT', 'MONEY',
                        'QUANTITY', 'CARDINAL', 'ORDINAL']
 
@@ -101,7 +96,7 @@ def extract_topic(author_names):
         for abstract in texts:
             doc = nlp(abstract)
             t = [token.lemma_.lower() for token in doc if token.is_alpha and token.ent_type_ not in remove_entities
-                 and token.lemma_.lower() not in stop_words and token.pos_ not in remove_pos and not token.is_stop]
+                 and token.lemma_.lower() not in STOP_WORDS and token.pos_ not in remove_pos and not token.is_stop]
             tokens.append(t)
 
         # add bigrams to the token list
