a3c434205504b056197547ea38f462ced1236fb0
ana_maria.nastase <ana_maria.nastase@stud.acs.upb.ro>
2024-06-03 16:33:20 +0300

    refactored code, modified the way stop words are removed

--- main.py
+++ main.py
@@ -11,14 +11,22 @@ import yake
 import fasttext
 import re
 
-STOP_WORDS = ['paper', 'present', 'propose', 'show', 'datum', 'people', 'result', 'solution', 'case', 'order',
-              'base', 'ieee', 'privacy', 'policy', 'new', 'old', 'context', 'high', 'different', 'research', 'type',
-              'approach', 'important', 'main', 'range', 'helpful', 'large', 'difficult', 'available', 'amount',
-              'useful', 'importance', 'article', 'abstract', 'scale', 'copyright', 'real', 'quality', 'demonstrate',
-              'inconvenience', 'benefit', 'unavailable', 'term', 'condition', 'interest', 'recent', 'obtain',
-              'title', 'jat', 'jats',
-              'organization', 'task', 'student', 'professor', 'teacher', 'university', 'workshop', 'study', 'text',
-              'conference']
+STOP_WORDS = ['abstract', 'amount', 'approach', 'article', 'available', 'base', 'based', 'benefit',
+              'bucharest',
+              'case', 'condition', 'conference', 'context', 'copyright', 'datum', 'demonstrate', 'demonstrates',
+              'demonstrated',
+              'different', 'difficult', 'experiment', 'experimental', 'faculty', 'helpful', 'high',
+              'ieee', 'importance', 'important', 'inconvenience', 'interest', 'interested', 'interests', 'jat',
+              'jats', 'laboratory',
+              'main', 'new', 'obtain', 'obtained', 'obtains', 'old', 'order', 'organization', 'paper', 'people',
+              'policy',
+              'politehnica', 'polytechnic',
+              'present', 'presents', 'presented', 'privacy', 'professor', 'propose', 'proposes', 'proposed',
+              'quality', 'range', 'ranges', 'real',
+              'recent', 'research', 'researcher', 'result', 'scale', 'show', 'shows', 'showed', 'student', 'study',
+              'studies', 'studied', 'task',
+              'teacher', 'term', 'text', 'title', 'type', 'unavailable', 'university', 'useful',
+              'workshop']
 
 
 def get_authors_texts():
@@ -28,113 +36,92 @@ def get_authors_texts():
     # load fastText model
     model = fasttext.load_model('lid.176.bin')
 
-    authors_texts = {author_id: "" for author_id in authors["id"]}
+    texts = {author_id: [] for author_id in authors["id"]}
     for author_id, abstract in author_publication_pairs:
         if abstract and isinstance(abstract, str) and re.match('^(?=.*[a-zA-Z])', abstract):
             predictions = model.predict(abstract)
             language = predictions[0][0].replace('__label__', '')
             if language == 'en':
-                authors_texts[author_id] = authors_texts[author_id] + '\n' + abstract
+                texts[author_id].append(abstract)
 
-    return authors_texts
+    return texts
 
 
-def extract_keywords(author_names):
-    """ extract keywords for each author using yake """
-    # make a dictionary containing the combined abstracts for each author
-    authors_texts = {}
-    for author_id, texts in get_authors_texts().items():
-        authors_texts[author_id] = '\n'.join(texts)
+def extract_keywords(abstract_list):
+    """ extract keywords from a list of abstracts using YAKE """
+    # concatenate the abstracts into a single string
+    text = '\n'.join(abstract_list)
 
     # load spacy model
-    nlp = spacy.load('en_core_web_md')
+    nlp = spacy.load('en_core_web_lg')
 
-    # entities/words to remove
+    # entities to remove
     remove_entities = ['PERSON', 'NORP', 'FAC', 'GPE', 'LOC', 'DATE', 'TIME', 'PERCENT', 'MONEY',
                        'QUANTITY', 'CARDINAL', 'ORDINAL']
 
-    for author_id, text in authors_texts.items():
-        if not text:
-            print(str(author_id) + " - " + author_names[author_id])
-            print("-")
-            continue
+    # preprocess text
+    nlp.max_length = len(text) + 1000
+    doc = nlp(text)
+    transformed_text = ' '.join(
+        [token.text for token in doc if token.ent_type_ not in remove_entities])
 
-        # preprocess text
-        nlp.max_length = len(text) + 100
-        doc = nlp(text)
-        transformed_text = ' '.join(
-            [token.text for token in doc if token.ent_type_ not in remove_entities
-             and token.lemma_.lower() not in STOP_WORDS])
+    # get 15 keyphrases of max 3 words
+    max_ngram = 3
+    deduplication_threshold = 0.5
+    keywords_nr = 15
+    windowsSize = 1
+    kw_extractor = yake.KeywordExtractor(lan="en", n=max_ngram, dedupLim=deduplication_threshold,
+                                         top=keywords_nr, windowsSize=windowsSize)
+    kw_extractor.stopword_set.update(set(STOP_WORDS))
 
-        # get 15 keyphrases of max 3 words
-        max_ngram = 3
-        deduplication_threshold = 0.5
-        keywords_nr = 15
-        custom_kw_extractor = yake.KeywordExtractor(lan="en", n=max_ngram, dedupLim=deduplication_threshold,
-                                                    top=keywords_nr, features=None)
-
-        keywords = custom_kw_extractor.extract_keywords(transformed_text)
-
-        print(str(author_id) + " - " + author_names[author_id])
-
-        for kw in keywords:
-            print(kw)
-
-        print()
+    kw = kw_extractor.extract_keywords(transformed_text)
+    return kw
 
 
-def extract_topic(author_names):
-    """ extract 1 topic containing 10 keywords for each author using LDA """
-    # make a dictionary containing the combined abstracts for each author
-    authors_texts = get_authors_texts()
-
+def extract_topic(abstract_list):
+    """ extract 1 topic containing 10 keywords from documents in abstract_list using LDA """
     # load spacy model
-    nlp = spacy.load('en_core_web_md')
+    nlp = spacy.load('en_core_web_lg')
 
     # keep only adjectives and nouns
     remove_pos = ['ADV', 'PRON', 'PART', 'DET', 'SPACE', 'NUM', 'SYM', 'ADP', 'VERB', 'CCONJ']
     remove_entities = ['PERSON', 'NORP', 'FAC', 'GPE', 'LOC', 'LANGUAGE', 'DATE', 'TIME', 'PERCENT', 'MONEY',
                        'QUANTITY', 'CARDINAL', 'ORDINAL']
 
-    for author_id, texts in authors_texts.items():
-        if not texts:
-            print(str(author_id) + " - " + author_names[author_id])
-            print("-")
-            continue
+    # preprocess documents
+    tokens = []
+    for abstract in abstract_list:
+        nlp.max_length = len(abstract) + 1000
+        doc = nlp(abstract)
+        t = [token.lemma_.lower() for token in doc if token.is_alpha and token.ent_type_ not in remove_entities
+             and token.lemma_.lower() not in STOP_WORDS and token.pos_ not in remove_pos and not token.is_stop]
+        tokens.append(t)
 
-        # preprocess texts
-        tokens = []
-        for abstract in texts:
-            nlp.max_length = len(abstract) + 100
-            doc = nlp(abstract)
-            t = [token.lemma_.lower() for token in doc if token.is_alpha and token.ent_type_ not in remove_entities
-                 and token.lemma_.lower() not in STOP_WORDS and token.pos_ not in remove_pos and not token.is_stop]
-            tokens.append(t)
+    # add bigrams to the token list
+    bigram = Phrases(tokens, min_count=2, delimiter=' ', threshold=1)
+    tokens = [bigram[text] for text in tokens]
+    trigram = Phrases(tokens, min_count=2, delimiter=' ', threshold=1)
+    tokens = [trigram[text] for text in tokens]
 
-        # add bigrams to the token list
-        bigram = Phrases(tokens, min_count=2, delimiter=' ', threshold=1)
-        tokens = [bigram[text] for text in tokens]
-        trigram = Phrases(tokens, min_count=2, delimiter=' ', threshold=1)
-        tokens = [trigram[text] for text in tokens]
+    # remove single words (if the word count after removal is at least 100)
+    ngrams = [[token for token in text if len(token.split(" ")) > 1] for text in tokens]
 
-        # remove single words
-        tokens = [[token for token in text if len(token.split(" ")) > 1] for text in tokens]
+    word_count = 0
+    for text in ngrams:
+        word_count += len(text)
 
-        # create dictionary with gensim
-        dictionary = Dictionary(tokens)
-        # create corpus
-        corpus = [dictionary.doc2bow(text) for text in tokens]
+    if word_count > 100:
+        tokens = ngrams
 
-        # apply LDA
-        lda_model = LdaMulticore(corpus=corpus, id2word=dictionary, iterations=500, num_topics=1, workers=4, passes=200)
-        topics = lda_model.print_topics(-1)
+    # create dictionary with gensim
+    dictionary = Dictionary(tokens)
+    # create corpus
+    corpus = [dictionary.doc2bow(text) for text in tokens]
 
-        print(str(author_id) + " - " + author_names[author_id])
-
-        for idx, topic in topics:
-            print(f"{idx}: {topic}")
-
-        print()
+    # apply LDA
+    lda_model = LdaMulticore(corpus=corpus, id2word=dictionary, iterations=300,
+                             num_topics=1, workers=4, passes=50)
+    return lda_model.print_topics(-1)
 
 
 if __name__ == '__main__':
@@ -147,7 +134,21 @@ if __name__ == '__main__':
     id_name_list = list(zip(authors['id'], authors['last_name'], authors['first_name']))
     author_names = {author_id: last_name + " " + first_name for author_id, last_name, first_name in id_name_list}
 
-    print("YAKE:")
-    extract_keywords(author_names)
-    print("\nLDA:")
-    extract_topic(author_names)
+    # make a dictionary containing a list of abstracts for each author
+    authors_texts = get_authors_texts()
+
+    # apply YAKE and LDA to find keywords for each author
+    for author_id, abstracts in authors_texts.items():
+        print(str(author_id) + " - " + author_names[author_id])
+
+        print("YAKE:")
+        keywords = extract_keywords(abstracts)
+        for k, _ in keywords:
+            print(k)
+
+        # print("\nLDA:")
+        # topics = extract_topic(abstracts)
+        # for idx, topic in topics:
+        #     print(f"{topic}")
+
+        print()
